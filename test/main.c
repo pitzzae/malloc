5bce5319fbbbf794ff2e4b48506223bc2424487b
pitzzae <gtorresa@student.42.fr>
2017-05-01 16:34:22 +0200

    Update test

--- test/main.c
+++ test/main.c
@@ -1,18 +1,66 @@
 #include "../includes/malloc.h"
+/* Pour les constantes EXIT_SUCCESS et EXIT_FAILURE */
+#include <stdlib.h>
+/* Pour fprintf() */
 #include <stdio.h>
+/* Pour fork() */
+#include <unistd.h>
+/* Pour perror() et errno */
+#include <errno.h>
+/* Pour le type pid_t */
+#include <sys/types.h>
+
+typedef struct		s_ptr
+{
+	char			*string1;
+	char			*string2;
+	char			*string3;
+	struct s_ptr	*next;
+}					t_ptr;
+
+void use_ram(void)
+{
+	t_ptr	*ptr;
+	t_ptr	*ptr_tmp;
+
+	ptr = malloc(sizeof(t_ptr));
+	ptr->string1 = malloc(sizeof(char) * 10000);
+	ptr->string1[0] = '\0';
+	ptr->string2 = malloc(sizeof(char) * 10000);
+	ptr->string2[0] = '\0';
+	ptr->string3 = malloc(sizeof(char) * 10000);
+	ptr->string3[0] = '\0';
+	ptr_tmp = ptr;
+	while ((ptr->next = malloc(sizeof(t_ptr))))
+	{
+		ptr->string1 = malloc(sizeof(char) * 10000);
+		ptr->string1[0] = '\0';
+		ptr->string2 = malloc(sizeof(char) * 10000);
+		ptr->string2[0] = '\0';
+		ptr->string3 = malloc(sizeof(char) * 10000);
+		ptr->string3[0] = '\0';
+		ptr = ptr->next;
+	}
+	while (ptr_tmp->next)
+	{
+		ptr = ptr_tmp->next;
+		free(ptr_tmp->string1);
+		free(ptr_tmp->string2);
+		free(ptr_tmp->string3);
+		free(ptr_tmp);
+		ptr_tmp = ptr;
+	}
+}
+
 
 int		main(void)
 {
-	char		*ptr;
+	pid_t pid[3];
 
-	(void)ptr;
-	printf("coucou\n");
-	ptr = malloc(99999);
-	ptr[0] = 'a';
-	ptr[1] = 'a';
-	ptr[2] = 'a';
-	ptr[3] = 'a';
-	printf("%s\n", ptr);
-	free(ptr);
-	//printf("%s\n", ptr);
+	(void)pid[0];
+	pid[0] = fork();
+	pid[1] = fork();
+	use_ram();
+	use_ram();
+	use_ram();
 }
